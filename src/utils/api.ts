1f85ceb77b6641e3297e91381dbca0c87867e46f
Godwin Mawunyo Addo <gssaddo@gmail.com>
2025-01-09 03:19:35 -0800

    done

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,5 @@
 import { ofetch } from 'ofetch'
+import axios from 'axios'
 
 export const $api = ofetch.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
@@ -6,5 +7,12 @@ export const $api = ofetch.create({
     const accessToken = useCookie('accessToken').value
     if (accessToken)
       options.headers.append('Authorization', `Bearer ${accessToken}`)
+  },
+})
+
+export const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_LOCAL_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
   },
 })
